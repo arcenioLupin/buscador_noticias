f99d448aa6955054c6baa4a7ebf58ae4d1746ee2
Arcenio Vilca Cáceres <avilca@csticorp.biz>
2019-11-18 21:54:34 -0500

    avance 18/11/2019

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -4,21 +4,26 @@ const Noticia = ({noticia}) => {
     
     //Extraer los datos
       const {urlToImage,url,title,description,source} = noticia;
+      const imagen = (urlToImage)?
+                    <div className="card-image">
+                        <img src={urlToImage} alt={title} />
+                        <span className="card-title">{source.name}</span>
+                    </div>:
+                    null;
 
     return (
         <div className="col s12 m6 l4">
             <div className="card">
-                <div className="card-image">
-                   <img src={urlToImage} alt={title} />
-                </div>
-
+                 {imagen} 
                 <div className="card-content">
                    <h3>{title}</h3>
                    <p>{description}</p>
                 </div>
 
                 <div className="card-action">
-                   <a href={url} target="_b"></a>
+                   <a href={url} target="_blank" rel="noopener noreferrer" className="waves-effect waves-light btn">
+                       Ver Noticia Completa
+                   </a>
                 </div>
             </div>
         </div>
